87ffaf09092af82b8dc5837b6dc2f10530fc6a9b
Shreyash Gavit <ssg1692@gmail.com>
2025-07-05 00:32:07 +0530

    Added animationSequance for both players

--- Scripts/playerMovement.gd
+++ Scripts/playerMovement.gd
@@ -1,27 +1,39 @@
 extends CharacterBody2D
 
-@export var curr_scene:Node2D
+@export var curr_scene:Node2D = null
 @export var controls:Resource = null
+@export var spriteControl:Sprite2D = null
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var JUMP_VELOCITY = -400.0
 func ready() -> void:
 	print(curr_scene.name)
-	if curr_scene.has_meta("Level_Type"):
-		match curr_scene.get_meta("Level_Type"):
-			"TopDown":
-				print("A top down Scene")
-			"Platformer":
-				print("A platformer Scene")
+	if has_meta("player_index"):
+		match curr_scene.get_meta("player_index"):
+			0:
+				JUMP_VELOCITY -= 100
+			1:
+				JUMP_VELOCITY = JUMP_VELOCITY + 100
 func apply_gravity(delta): 
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+func _process(delta: float) -> void:
+	if(velocity.x>0):
+		# walking Right
+		spriteControl.flip_h = false
+	elif(velocity.x<0):
+		# walking left
+		spriteControl.flip_h = true
+	else:
+		# IDeal
+		pass
+
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
 	# Handle jump.
-	if Input.is_action_just_pressed(controls.jump) and is_on_floor():
+	if (Input.is_action_just_pressed(controls.jump) or Input.is_action_just_pressed(controls.move_up)) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
