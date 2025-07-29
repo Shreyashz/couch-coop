5f5847a5ea55dc95dfc742ac4e3dd4d68d9efddb
Cesar-caza-epng <159482256+Cesar-caza-epng@users.noreply.github.com>
2025-07-28 21:49:18 -0700

    MORE LEVELS!

--- togglable_Wall.gd
+++ togglable_Wall.gd
@@ -1,6 +1,11 @@
 extends StaticBody2D
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@export var Start_Disabled = false
+
+func _ready() -> void:
+	if(Start_Disabled):
+		toggle()
 
 func toggle():
 	call_deferred("_do_toggle")
